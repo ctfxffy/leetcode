45a205b4d78e97a3f821a45b26bec4c758fad993
gxy <719618847@sjtu.edu.cn>
2026-08-07 20:40:07 +0800

    feat(leetcode): 新增5道LeetCode数组与贪心算法题的题解及题目文档
    本次新增覆盖121买卖股票的最佳时机、122买卖股票的最佳时机II、189轮转数组、55跳跃游戏、45跳跃游戏II，均附带Python实现题解和完整中文题目说明。

---
+++ 跳跃游戏II/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        steps = 0
+        current_end = 0
+        farthest = 0
+
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+
+            if i == current_end:
+                steps += 1
+                current_end = farthest
+
+        return steps
