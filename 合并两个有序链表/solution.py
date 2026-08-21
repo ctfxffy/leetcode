f435c8d46c1056070085b89035ccdbf3d277b58f
gxy <719618847@sjtu.edu.cn>
2026-08-21 16:02:16 +0800

    feat(leetcode链表题): 添加三道链表算法题的文档和Python题解
    分别新增141.环形链表、21.合并两个有序链表、2.两数相加的题目描述与对应Python实现代码

---
+++ 合并两个有序链表/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0)
+        cur = dummy
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+
+        if l1:
+            cur.next = l1
+        else:
+            cur.next = l2
+
+        return dummy.next
